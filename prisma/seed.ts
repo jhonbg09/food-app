7ccbb0d8ff3bf9282bf00627ee63447900987012
jhonbg09 <jhon.betancourthg09@gmail.com>
2024-11-17 20:14:41 -0500

    Agragando script del seed en el package.json

---
+++ prisma/seed.ts
@@ -0,0 +1,19 @@
+import { categories } from "./data/categories";
+import { products } from "./data/products";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+async function main(){
+    try {
+        await prisma.category.createMany({
+            data: categories
+        })
+
+        await prisma.product.createMany({
+            data: products
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
